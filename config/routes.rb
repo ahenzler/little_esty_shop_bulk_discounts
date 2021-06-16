945eaca1f43fd4280fd2c6b8ce131980b6b5a363
ahenzler <59706432+ahenzler@users.noreply.github.com>
2021-06-16 00:38:20 -0600

    add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/', to: 'application#welcome'
+
   resources :discounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :merchant, only: [:show] do
