bbea3ec7df6b9fd867db8f8e255708219517ea67
ahenzler <59706432+ahenzler@users.noreply.github.com>
2021-07-16 10:16:04 -0600

    fixes

--- spec/requests/welcome_request_spec.rb
+++ spec/requests/welcome_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Welcomes", type: :request do
 
   describe "GET /index" do
     it "returns http success" do
-      get "/welcome/index"
+      root "/welcome/index"
       expect(response).to have_http_status(:success)
     end
   end
